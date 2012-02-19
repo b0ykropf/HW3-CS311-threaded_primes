62a5bbbae308378d24c1284f50f768e557e45248
Byron Kropf <b0ykropf@yahoo.com>
2012-02-18 16:01:31 -0800

    Progressed on logic for using bitmaps with masks. Still need to work in complete seive logic and finished bitmap logic.

--- prime_threads.cpp
+++ prime_threads.cpp
@@ -5,10 +5,14 @@
 #include <pthread.h>
 
 
-#define MAX_VALUE 80  //4294967296
+#define MAX_VALUE 81  //4294967296
 #define NUM_THREADS 4
 
 using namespace std;
+
+long bits_set = 0;
+unsigned char bitmap[10];
+unsigned char bitmask[8] = {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
 
 struct thread_data{
     int prime;
@@ -21,19 +25,22 @@ void *thread_prime(void *thread_args){
 
     int my_name;
     int my_number;
-
+    int seek_byte;
+    int seek_bit;
     struct thread_data *cur_data;
     cur_data = (struct thread_data *) thread_args;
     my_name = cur_data->thread_name;
     my_number = cur_data->prime;
-
     cout << "I am thread #" << my_name << endl;
     cout << "My number is #" << my_number << endl;
 
-    for(int i = my_number; i < MAX_VALUE; ++i){
-        if((i % my_number)==0){
-            cout << "I woud set bit # : " << i << endl;    
-        }
+    for(int i = my_number + my_number; i < MAX_VALUE; i += my_number){
+           cout << "Thread #" << my_name << " would set bit # : " << i << endl;
+           seek_byte = i / 8;
+           seek_bit = i % 8;
+           bitmap[seek_byte] |= bitmask[seek_bit];
+           ++bits_set;    
+        
     }
     pthread_exit(NULL);    
 }
@@ -43,7 +50,7 @@ int main(){
 
     cout << "I'm Here! \n";
 
-    int num_nums = MAX_VALUE;
+    unsigned long num_nums = MAX_VALUE;
     int rc;
     int t = 0;
     int num_comp = 0;
@@ -53,18 +60,15 @@ int main(){
 
     pthread_t threads[NUM_THREADS];
 
-    unsigned char bitmap[10];
-    unsigned char bitmask[8] = {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
-    while(next_num < 20){
+    while(next_num < 6){
        
         for(t = 0; t < NUM_THREADS; ++t){
             cout << "Stringing thread: " << t << endl;
             thread_data_array[t].thread_name = t;
             thread_data_array[t].prime = next_num;
-            next_num++;
+            ++next_num;
             rc = pthread_create(&threads[t],NULL,thread_prime,(void *) &thread_data_array[t]);
-            live_threads++;
-            sleep(1);
+            ++live_threads;
         }
 	for(t = 0;t < NUM_THREADS; ++t){
             pthread_join(threads[t], NULL);
@@ -74,5 +78,9 @@ int main(){
  
     }
 
+if(bitmap[1] & bitmask[4]){
+    cout << "Bit # 4 has been set" << bitmap[1] << endl; 
+}
+cout << "bits_set " << bits_set << endl;
 return 0;
 }
